4d79cdfbe1732e87e2e80a155f7ed51ac347bde1
Luciekimotho <luciekimotho@gmail.com>
2015-10-21 19:10:37 +0300

    Added the comment box

--- quorahome/forms.py
+++ quorahome/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Question
+from .models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         fields = ['question']
         model = Question
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        fields = ['answer']
+        model = Answer
+
--- quorahome/views.py
+++ quorahome/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 # Create your views here.
 
 def questions(request):
     questions = Question.objects.all()
-    context = {'questions':questions}
+    form = AnswerForm(request.POST or None)
+    if form.is_valid():
+        answers = form.save(commit=False)
+        answers.questions = questions
+        answers.save()
+        return redirect(request.path)
+    context = {'questions':questions,
+               'form': form
+              }
     return render(request, 'questions.html', context)
 
 def home(request):
@@ -19,3 +27,4 @@ def home(request):
         instance = form.save(commit = False)
         instance.save()
     return render(request, 'quorahome.html', context)
+
