905a7178ca2dbb9ccb80b163ecd24276e0fd5e4e
Luciekimotho <luciekimotho@gmail.com>
2015-10-21 14:46:22 +0300

    created the view for the questions

--- quorahome/views.py
+++ quorahome/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .models import Question
 # Create your views here.
+
+def questions(request):
+    context = {}
+    return render(request, 'questions.html', context)
