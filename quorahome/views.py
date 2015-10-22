536131fc5048837f6305464957844d46a56c8bfe
Luciekimotho <luciekimotho@gmail.com>
2015-10-23 00:12:54 +0300

    Comment views seen

--- quorahome/views.py
+++ quorahome/views.py
@@ -4,11 +4,6 @@ from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 # Create your views here.
 
-def questions(request):
-    questions = Question.objects.all()
-    context = { 'questions' : questions}
-    return render(request, 'questions.html', context)
-
 def home(request):
     form = QuestionForm(request.POST or None)
     print (request.POST)
@@ -20,9 +15,16 @@ def home(request):
         instance.save()
     return render(request, 'quorahome.html', context)
 
+def questions(request):
+    questions = Question.objects.all()
+    context = { 'questions' : questions}
+    return render(request, 'questions.html', context)
+
 def viewquestion(request, pk):
     questions = get_object_or_404(Question, pk=pk)
+    answers = Answer.objects.all()
     context = {
-        'questions':questions
+        'questions':questions,
+        'answers':answers
     }
     return render(request, 'viewquestion.html', context)
